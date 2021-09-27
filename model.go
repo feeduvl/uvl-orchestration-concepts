716ade210946200588f5d88a1cd9635d1e60764c
Jakob Weichselbaumer <j.weichse@gmail.com>
2021-09-27 15:22:24 +0200

    Update Annotation model add last_updated

--- model.go
+++ model.go
@@ -36,9 +36,11 @@ type Token struct {
 }
 
 type Annotation struct {
-	UploadedAt time.Time `validate:"nonzero" json:"uploaded_at" bson:"uploaded_at"`
-	Name       string    `validate:"nonzero" json:"name" bson:"name"`
-	Dataset    string    `validate:"nonzero" json:"dataset" bson:"dataset"`
+	UploadedAt  time.Time `validate:"nonzero" json:"uploaded_at" bson:"uploaded_at"`
+	LastUpdated time.Time `json:"last_updated" bson:"last_updated"`
+
+	Name    string `validate:"nonzero" json:"name" bson:"name"`
+	Dataset string `validate:"nonzero" json:"dataset" bson:"dataset"`
 
 	Docs              []DocWrapper       `json:"docs" bson:"docs"`
 	Tokens            []Token            `json:"tokens" bson:"tokens"`
